cc414d6ac14e1b39f041dda2f2475e1651a366bf
huynd2174 <126533279+huynd2174@users.noreply.github.com>
2025-02-18 20:38:26 +0700

    Add files via upload

---
+++ ex2/src/main/java/com/example/ex2/repository/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.example.ex2.repository;
+
+import com.example.ex2.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+}
